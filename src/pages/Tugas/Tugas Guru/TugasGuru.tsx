5111fdced6bcfc98bd9e8926e341f901d26a38cd
zvn7 <aammaul7@gmail.com>
2024-03-28 11:03:32 +0700

    Update form tugas

--- src/pages/Tugas/Tugas Guru/TugasGuru.tsx
+++ src/pages/Tugas/Tugas Guru/TugasGuru.tsx
@@ -252,7 +252,7 @@ const TugasGuru = () => {
 											d="M5 12h14m-7 7V5"
 										/>
 									</svg>
-									Materi
+									Tugas
 								</button>
 							)}
 							{isTablet && (
@@ -278,7 +278,7 @@ const TugasGuru = () => {
 											d="M5 12h14m-7 7V5"
 										/>
 									</svg>
-									Materi
+									Tugas
 								</button>
 							)}
 							{!isMobile && !isTablet && (
@@ -304,7 +304,7 @@ const TugasGuru = () => {
 											d="M5 12h14m-7 7V5"
 										/>
 									</svg>
-									Materi
+									Tugas
 								</button>
 							)}
 						</div>
@@ -438,19 +438,18 @@ const TugasGuru = () => {
 								<form className="max-w-full mt-4">
 									<div className="mb-5">
 										<label
-											htmlFor="email"
+											htmlFor="materi"
 											className="block mb-2 text-sm font-medium text-blue-600 dark:text-white"
 										>
-											Mata Pelajaran
+											Materi
 										</label>
 										<select
 											id="countries"
 											className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 										>
-											<option>Mata Pelajaran</option>
-											<option>Canada</option>
-											<option>France</option>
-											<option>Germany</option>
+											<option>Materi</option>
+											<option>Desain</option>
+											<option>Tools</option>
 										</select>
 									</div>
 									<div className="mb-5">
@@ -603,19 +602,18 @@ const TugasGuru = () => {
 									<form className="max-w-full mt-4">
 										<div className="mb-5">
 											<label
-												htmlFor="email"
+												htmlFor="materi"
 												className="block mb-2 text-sm font-medium text-blue-600 dark:text-white"
 											>
-												Mata Pelajaran
+												Materi
 											</label>
 											<select
 												id="countries"
 												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											>
-												<option>Mata Pelajaran</option>
-												<option>Canada</option>
-												<option>France</option>
-												<option>Germany</option>
+												<option>Materi</option>
+												<option>Desain</option>
+												<option>Tools</option>
 											</select>
 										</div>
 										<div className="mb-5">
@@ -749,19 +747,18 @@ const TugasGuru = () => {
 									<form className="max-w-full">
 										<div className="mb-5">
 											<label
-												htmlFor="email"
+												htmlFor="materi"
 												className="block mb-2 text-sm font-medium text-blue-600 dark:text-white"
 											>
-												Mata Pelajaran
+												Materi
 											</label>
 											<select
 												id="countries"
 												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											>
-												<option>Mata Pelajaran</option>
-												<option>Canada</option>
-												<option>France</option>
-												<option>Germany</option>
+												<option>Materi</option>
+												<option>Desain</option>
+												<option>Tools</option>
 											</select>
 										</div>
 										<div className="mb-5">
@@ -895,19 +892,18 @@ const TugasGuru = () => {
 									<form className="max-w-full">
 										<div className="mb-5">
 											<label
-												htmlFor="email"
+												htmlFor="materi"
 												className="block mb-2 text-sm font-medium text-blue-600 dark:text-white"
 											>
-												Mata Pelajaran
+												Materi
 											</label>
 											<select
 												id="countries"
 												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											>
-												<option>Mata Pelajaran</option>
-												<option>Canada</option>
-												<option>France</option>
-												<option>Germany</option>
+												<option>Materi</option>
+												<option>Desain</option>
+												<option>Tools</option>
 											</select>
 										</div>
 										<div className="mb-5">
@@ -1041,19 +1037,18 @@ const TugasGuru = () => {
 									<form className="max-w-full">
 										<div className="mb-5">
 											<label
-												htmlFor="email"
+												htmlFor="materi"
 												className="block mb-2 text-sm font-medium text-blue-600 dark:text-white"
 											>
-												Mata Pelajaran
+												Materi
 											</label>
 											<select
 												id="countries"
 												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											>
-												<option>Mata Pelajaran</option>
-												<option>Canada</option>
-												<option>France</option>
-												<option>Germany</option>
+												<option>Materi</option>
+												<option>Desain</option>
+												<option>Tools</option>
 											</select>
 										</div>
 										<div className="mb-5">
@@ -1187,19 +1182,18 @@ const TugasGuru = () => {
 									<form className="max-w-full">
 										<div className="mb-5">
 											<label
-												htmlFor="email"
+												htmlFor="materi"
 												className="block mb-2 text-sm font-medium text-blue-600 dark:text-white"
 											>
-												Mata Pelajaran
+												Materi
 											</label>
 											<select
 												id="countries"
 												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 											>
-												<option>Mata Pelajaran</option>
-												<option>Canada</option>
-												<option>France</option>
-												<option>Germany</option>
+												<option>Materi</option>
+												<option>Desain</option>
+												<option>Tools</option>
 											</select>
 										</div>
 										<div className="mb-5">
