d3b0732087afe1736a0e6af4bc2680a009410fe1
zvn7 <aammaul7@gmail.com>
2024-05-26 16:32:13 +0700

    edit env

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
-import path from "path";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-			workbox: {
-				globPatterns: ["**/*"],
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+	manifest: {
+		name: "Edutech",
+		short_name: "Edutech",
+		description: "Education Platform",
+		icons: [
+			{
+				src: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
 			},
-			includeAssets: ["**/*"],
-			manifest: {
-				theme_color: "#1BC000",
-				background_color: "#1BC000",
-				display: "standalone",
-				scope: "/",
-				start_url: "/",
-				short_name: "Finthest",
-				description: "Finthest",
-				name: "Finthest",
-				icons: [
-					{
-						src: "/android-chrome-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "/android-chrome-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "/apple-touch-icon.png",
-						sizes: "180x180",
-						type: "image/png",
-						purpose: "apple touch icon",
-					},
-					{
-						src: "/maskable_icon.png",
-						sizes: "225x225",
-						type: "image/png",
-						purpose: "maskable",
-					},
-				],
+			{
+				src: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
 			},
-		}),
-	],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "/maskable_icon.png",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "maskable",
+			},
+		],
+		theme_color: "#171717",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
 	},
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+	plugins: [react(), VitePWA(manifestForPlugin)],
 });
