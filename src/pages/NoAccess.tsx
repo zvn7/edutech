ccc5b9d905332492c0da515d126d89c5d6708a00
zvn7 <aammaul7@gmail.com>
2024-05-31 13:12:16 +0700

    change notfound to no access

--- src/pages/NoAccess.tsx
+++ src/pages/NoAccess.tsx
@@ -1,14 +1,15 @@
-const NotFound = () => {
+const NoAccess = () => {
 	return (
 		<div className="bg-gradient-to-br from-purple-800 to-blue-600 text-white min-h-screen flex items-center">
 			<div className="container mx-auto p-4 flex flex-wrap items-center justify-center">
 				<div className="w-full md:w-7/12 text-center md:text-left p-4">
-					<div className="text-6xl font-medium">404</div>
+					<div className="text-6xl font-medium">403</div>
 					<div className="text-xl md:text-3xl font-medium mb-4">
-						Oops. This page has gone missing.
+						You don't have access to this page.
 					</div>
 					<div className="text-lg mb-8">
-						You may have mistyped the address or the page may have moved.
+						Please check your permissions or contact the site administrator if
+						you believe this is a mistake.
 					</div>
 				</div>
 			</div>
@@ -16,4 +17,4 @@ const NotFound = () => {
 	);
 };
 
-export default NotFound;
+export default NoAccess;
